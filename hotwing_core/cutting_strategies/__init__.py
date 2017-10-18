336c274a1531723b1ecdcdaa76f829996892b1f4
Jason Hamilton <jasonscotthamilton@gmail.com>
2017-10-18 16:23:18 -0700

    Fixes failing build on python 3 due to non-explicit relative import

--- hotwing_core/cutting_strategies/__init__.py
+++ hotwing_core/cutting_strategies/__init__.py
@@ -1,4 +1,4 @@
-from base import CuttingStrategyBase
+from .base import CuttingStrategyBase
 from .cutting_strategy_1 import CuttingStrategy1
 from .cutting_strategy_2 import CuttingStrategy2
 from .factory import CuttingStrategyFactory

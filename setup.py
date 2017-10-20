9de1dc185bc3b6e997f9957dd6adfa20c274d807
Jason Hamilton <jasonscotthamilton@gmail.com>
2017-10-20 13:01:34 -0700

    Updates setup.py

--- setup.py
+++ setup.py
@@ -16,22 +16,18 @@ setup(
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
-        #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
 
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
 
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
-        # 'Programming Language :: Python :: 2.6',
-        # 'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        # 'Programming Language :: Python :: 3.2',
-        # 'Programming Language :: Python :: 3.3',
-        # 'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
     ],
 )
